c1e98c416baee3c5337da9b4e8307ff9a2733004
OrbitalCountryFlags <Jeremy.bertrand17175@outlook.com>
2026-04-21 19:22:54 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with Astral Fire",
+        name: "Digital Dynasty Squad Community",
         // Activity type number (0 = Playing).
         type: 0, 
       },
